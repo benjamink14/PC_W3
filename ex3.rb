75df9bcda2946e7d3d61ff727a3c997c3953e775
Benjamin Kollek <benkollek@gmail.com>
2015-03-03 12:39:31 +0200

    Changed ex3.rb

--- ex3.rb
+++ ex3.rb
@@ -3,6 +3,8 @@ puts "I will now count my chickens:"
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+#This is a comment
+
 puts "Now I'll count the eggs:"
 puts 3 + 2 +1 -5 +4 % 3 + 5 /8
 
