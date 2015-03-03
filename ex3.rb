f1d9f4e5c12503f19342252046b429adc9fa2f74
Benjamin Kollek <benkollek@gmail.com>
2015-03-03 12:12:10 +0200

    Initial Commit

---
+++ ex3.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+puts "I will now count my chickens:"
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I'll count the eggs:"
+puts 3 + 2 +1 -5 +4 % 3 + 5 /8
+
+puts 100.38 * 67.3 / 43.20987
