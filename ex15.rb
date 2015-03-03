ffdcfdb6099c52bfb65f958db7fbfcc7298e7f58
Benjamin Kollek <benkollek@gmail.com>
2015-03-03 14:31:28 +0200

    Added exercise15

---
+++ ex15.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "This is your text file: #{filename}"
+puts txt.read
+
+print "Type the file name again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
